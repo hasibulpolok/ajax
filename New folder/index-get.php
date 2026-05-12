026c1df7d63fae49fd4108886cd852f07020a664
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-05-12 13:06:11 +0600

    ajax req

---
+++ New folder/index-get.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajsx get</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+</head>
+
+<body>
+    <button id="btn">
+        Load Data
+    </button>
+    <div id="result"></div>
+
+    <script>
+        $(document).ready(function () {
+            $('#btn').click(function () {
+                $.get("data.txt", function (data) {
+                    $('#result').text(data);
+                })
+            })
+        })
+
+
+    </script>
+
+</body>
+
+</html>
---
+++ store.php
@@ -0,0 +1,4 @@
+<?php 
+$name = $_POST['name'];
+echo "This is " . $name;
+?>
